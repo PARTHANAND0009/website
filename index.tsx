ae9f5749b6c7f4b17df89fe86c431d3d0cb84c51
PARTHANAND0009 <142311789+PARTHANAND0009@users.noreply.github.com>
2026-01-07 11:48:38 +0530

    feat: Conditionally render React app
    Avoids errors by only attempting to mount the React application if the '#root' element is found in the DOM.

--- index.tsx
+++ index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Only attempt to render React if the root element exists
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
